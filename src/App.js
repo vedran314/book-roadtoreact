83e87ebef89f6357c2b1f6e55cafb09437501ba8
Vedran <vedran.dev@gmail.com>
2018-09-19 13:47:28 +0200

    Chapter_2/Event_Handler added event handling inlined

--- src/App.js
+++ src/App.js
@@ -41,24 +41,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item => (
-          <div key="item.objectID">
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
-                Dismiss
-              </button>
-            </span>
-          </div>
-        ))}
+        {this.state.list.map(item => {
+          return (
+            <div key="item.objectID">
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
+            </div>
+          );
+        })}
       </div>
     );
   }
